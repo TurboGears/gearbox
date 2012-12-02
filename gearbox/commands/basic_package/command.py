a055b20eab4057bb591d3f72fee8811531d80f49
Alessandro Molina <alessandro.molina@axant.it>
2012-12-02 01:36:07 +0100

    Implement various options for the makepackage command

--- gearbox/commands/basic_package/command.py
+++ gearbox/commands/basic_package/command.py
@@ -9,21 +9,52 @@ class MakePackageCommand(TemplateCommand):
     def get_parser(self, prog_name):
         parser = super(MakePackageCommand, self).get_parser(prog_name)
 
-        parser.add_argument("name")
+        parser.add_argument('-n', '--name', dest='project',
+                            metavar='NAME', required=True,
+                            help="Project Name")
+
+        parser.add_argument('-o', '--output-dir', dest='output_dir',
+                            metavar='OUTPUT_DIR',
+                            help="Destination directory (by default the project name)")
+
+        parser.add_argument('-p', '--package', dest='package',
+                            metavar='PACKAGE',
+                            help="Python Package Name")
+
+        parser.add_argument('-a', '--author', dest='author',
+                            metavar='AUTHOR', default='Unknown',
+                            help="Name of the package author")
+
+        parser.add_argument('-e', '--email', dest='author_email',
+                            metavar='AUTHOR_EMAIL',
+                            help="Email of the package author")
+
+        parser.add_argument('-u', '--url', dest='url',
+                            metavar='URL',
+                            help="Project homepage")
+
+        parser.add_argument('-l', '--license', dest='license_name',
+                            metavar='LICENSE_NAME',
+                            help="License used for the project")
+
+        parser.add_argument('-d', '--description', dest='description',
+                            metavar='DESCRIPTION',
+                            help="Package description")
+
+        parser.add_argument('-k', '--keywords', dest='keywords',
+                            metavar='KEYWORDS',
+                            help="Package keywords")
 
         return parser
 
     def take_action(self, opts):
-        opts.project = opts.name
-        opts.package = opts.name
-        opts.author = 'Unknown'
-        opts.author_email = None
-        opts.url = None
-        opts.license_name = None
-        opts.version = '0.0.1'
-        opts.description = None
-        opts.long_description = None
-        opts.keywords = None
-        opts.zip_safe = False
+        if opts.package is None:
+            opts.package = opts.project
 
-        self.run_template(opts.name, opts)
+        if opts.output_dir is None:
+            opts.output_dir = opts.project
+
+        opts.zip_safe = False
+        opts.version = '0.0.1'
+
+        self.run_template(opts.output_dir, opts)
